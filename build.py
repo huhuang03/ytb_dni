a9824e5eb328193698eb4c56e53361682bfadb69
huhuang03 <huhuang03@gmail.com>
2023-11-21 19:46:50 +0800

    up

--- build.py
+++ build.py
@@ -6,6 +6,13 @@ suffix = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
 zf = zipfile.ZipFile('src-{}.zip'.format(suffix), 'w')
 
+# remove all exist zip
+
+cur_root = os.path.dirname(__file__)
+for name in os.listdir(cur_root):
+    if name.startswith('src-') and name.endswith('.zip'):
+        os.remove(os.path.join(cur_root, name))
+
 for dirname, subdirs, files in os.walk('dist'):
     zf.write(dirname)
     for filename in files:
