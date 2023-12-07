14ab01e2cbc2c66d3efff7c145a659268bc6665e
huhuang03 <huhuang03@gmail.com>
2023-12-08 07:37:02 +0800

    fix: fix publish.py, which should remove all exists first, then create new

--- publish.py
+++ publish.py
@@ -4,14 +4,14 @@ import zipfile
 
 suffix = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-zf = zipfile.ZipFile('src-{}.zip'.format(suffix), 'w')
-
 # remove all exist zip
 
 cur_root = os.path.dirname(__file__)
 for name in os.listdir(cur_root):
     if name.startswith('src-') and name.endswith('.zip'):
         os.remove(os.path.join(cur_root, name))
+
+zf = zipfile.ZipFile('src-{}.zip'.format(suffix), 'w')
 
 for dirname, subdirs, files in os.walk('dist'):
     zf.write(dirname)
