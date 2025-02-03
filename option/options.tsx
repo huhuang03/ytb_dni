bb8699bbae94abc79d1ef8f6d56d507625ee2542
huhuang03 <huhuang03@gmail.com>
2025-02-03 18:05:03 +0800

    up

---
+++ option/options.tsx
@@ -0,0 +1,39 @@
+// import {KEY_TELL_US_WHY} from '../src/common/constants';
+//
+// const CB_ID = 'cbClickTellNotInterest'
+// declare var chrome: any
+//
+// const sync = (value, callback) => {
+//   chrome.storage.local.get(
+//     {[KEY_TELL_US_WHY]: value},
+//     (items) => {
+//       const hasValue = value !== null && value !== undefined
+//       let tell = items[KEY_TELL_US_WHY]
+//       if (hasValue) {
+//         tell = !!value
+//       }
+//       tell = !!tell
+//       const cb = document.getElementById(CB_ID) as HTMLInputElement
+//       cb.checked = tell
+//       if (value !== null && value !== undefined) {
+//         chrome.storage.local.set({
+//           [KEY_TELL_US_WHY]: tell
+//         })
+//       }
+//       callback && callback()
+//     }
+//   );
+// }
+//
+// sync(null, () => {
+//   const cb = document.getElementById(CB_ID);
+//   cb.addEventListener('change', (ev) => {
+//     const target = ev.target as HTMLInputElement
+//     sync(target.checked, null);
+//   });
+// });
+//
+import {createRoot} from 'react-dom/client';
+
+const root = createRoot(document.getElementById("id"))
+root.render(<h1>Hello World</h1>)
