1359496c5aa6c591ebf011b555e47047e0de6f8a
Aha <huhuang03@gmail.com>
2026-01-21 23:13:57 +0800

    feat: handly update the css (#21)
    * feat: remove manually added css, use builtin css
    
    * fix: fix dni button css

--- src/dni.ts
+++ src/dni.ts
@@ -17,7 +17,7 @@ export class DNI extends HtmlElementWrapper {
   // it's something not interesting??
   static _createDniButton(marginTop: number) {
     let button = document.createElement('button');
-    button.setAttribute('class', 'style-scope yt-icon-button ytd-menu-renderer')
+    button.setAttribute('class', 'yt-spec-button-shape-next yt-spec-button-shape-next--mono yt-spec-button-shape-next--text yt-spec-button-shape-next__icon yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-button')
     button.setAttribute('style', `margin-top: ${marginTop}px`)
 
     let svg = DNI._createSvg()
